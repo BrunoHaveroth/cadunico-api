d1eecb98a8aa337c76620b853c719cf59e1da742
Bruno Haveroth <brun00haveroth@gmail.com>
2016-11-26 12:04:50 -0200

    populate db

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -60,7 +60,7 @@ module.exports = {
       });
   },
 
-	populatePerson: function() {
+	populatePerson2: function() {
 		var model = {
 			city: "Cidade Teste",
 			zipCode: "56678888",
@@ -77,23 +77,66 @@ module.exports = {
 			gender: "Masculino",
 			skinColor: "Preta",
 			motherName: "Joana teste",
-			fatherName: "Mario teste",
-			keepImage: true
+			fatherName: "Mario teste"
 		};
 
-		for (var i = 1; i <= 20; i++) {
-			var Chance = require('chance'),
+		var names = ['Aatrox', 'Ahri', 'Akali', 'Alistar', 'Amumu', 'Anivia', 'Annie', 'Ashe', 'Azir', 'Blitzcrank', 'Brand', 'Braum', 'Caitlyn', 'Cassiopeia', 'Corki', 'Darius', 'Diana', 'Draven',
+		 'Ekko', 'Elise', 'Evelynn', 'Ezreal', 'Fiora', 'Fizz', 'Galio', 'Gangplank', 'Garen', 'Gnar', 'Gragas', 'Graves', 'Hecarim', 'Heimerdinger', 'Illaoi', 'Irelia', 'Ivern', 'Janna', 'Jax',
+		 'Jayce', 'Jhin', 'Jinx', 'Kalista', 'Karma', 'Karthus', 'Kassadin', 'Katarina', 'Kayle', 'Kennen', 'Kindred', 'Leona', 'Lissandra', 'Lucian', 'Lulu', 'Lux', 'Malphite', 'Malzahar', 'Maokai',
+		 'Mordekaiser', 'Morgana', 'Nami', 'Nasus', 'Nautilus', 'Nidalee', 'Nocturne', 'Nunu', 'Olaf', 'Orianna', 'Pantheon', 'Poppy', 'Quinn', 'Rammus', 'Renekton', 'Rengar', 'Riven', 'Rumble', 'Ryze', 'Sejuani',
+		 'Shaco', 'Shen', 'Shyvana', 'Singed', 'Sion', 'Sivir', 'Skarner', 'Sona', 'Soraka', 'Swain', 'Syndra', 'Taliyah', 'Talon', 'Taric', 'Teemo', 'Thresh', 'Tristana', 'Trundle', 'Tryndamere', 'Twitch',
+		 'Udyr', 'Urgot', 'Varus', 'Vayne', 'Veigar', 'Vi', 'Viktor', 'Vladimir', 'Volibear', 'Warwick', 'Xerath', 'Yasuo', 'Yorick', 'Zac', 'Zed', 'Ziggs', 'Zilean', 'Zyra'];
+
+		var Chance = require('chance'),
+    chance = new Chance();
+
+	  names.forEach(function(item, index) {
+			var newPerson = model;
+			newPerson.fullName = chance.name();
+			newPerson.steganoImage = 'assets/images/' + item + '.png';
+
+			Person
+			.create(model)
+			.then();
+			console.log('>> '+ index);
+
+	  });
+	},
+
+	populatePerson: function() {
+	  var model = {
+	    city: "Cidade Teste",
+	    zipCode: "56678888",
+	    locality: "Bairro teste",
+	    publicPlace: "Logradouro teste",
+	    complement: "AP",
+	    characteristics: "Urbana",
+	    typeHousehold: "Particular Improvisado",
+	    typeWaterSupply: "Rede geral",
+	    toiletBathroom: "Sim",
+	    lightingUsed: "Elétrica com medidor comunitário",
+	    familyType: "Indígena",
+	    fullName: "Teste Da Silva",
+	    gender: "Masculino",
+	    skinColor: "Preta",
+	    motherName: "Joana teste",
+	    fatherName: "Mario teste",
+	    keepImage: true
+	  };
+
+	  for (var i = 1; i <= 20; i++) {
+	    var Chance = require('chance'),
 	    chance = new Chance();
 
-				var newPerson = model;
-				newPerson.fullName = chance.name();
-				newPerson.steganoImage = 'assets/images/' + i + '.png';
+	      var newPerson = model;
+	      newPerson.fullName = chance.name();
+	      newPerson.steganoImage = 'assets/images/' + i + '.png';
 
-				Person
-				.create(model)
-				.then();
-				console.log('>> '+ i);
+	      Person
+	      .create(model)
+	      .then();
+	      console.log('>> '+ i);
 
-		}
+	  }
 	}
 };
